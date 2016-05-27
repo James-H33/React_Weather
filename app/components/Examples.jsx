705ee9edd812dc081a4df71eb8afffd0ace195c0
James Hall <llahwj33@gmail.com>
2016-05-26 21:37:00 -0400

    Styled the Main Component and Examples Component.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function() {
@@ -9,7 +10,20 @@ var React = require('react');
 // });
 
 var Examples = (props) => {
-  return <h3> Examples </h3>
+  return (
+    <div>
+      <h1 className="text-center">Examples </h1>
+      <p>Here are a few locaitions to try out: </p>
+      <ol>
+        <li>
+          <Link to="/?location=Tampa"> Tampa</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio"> Rio</Link>
+        </li>
+      </ol>
+  </div>
+  );
 }
 
 
